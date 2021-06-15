f753aa54b71b65d922f7015ae7445293c01e5f57
RushMMC <2759889206@qq.com>
2021-06-15 16:44:43 +0800

    Add Dao

---
+++ jwxt/src/dao/DBCPUtil.java
@@ -0,0 +1,63 @@
+package dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
+
+public class DBCPUtil {
+
+	private static DataSource ds = null;
+	static {
+		Properties prop = new Properties();
+		try {
+			prop.load(new DBCPUtil().getClass().getClassLoader().getResourceAsStream("dbcpconfig.properties"));
+			ds = BasicDataSourceFactory.createDataSource(prop);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		};
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return ds.getConnection();
+	}
+	
+	public static void release(Connection conn,PreparedStatement pstmt) {
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			pstmt = null;
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			conn = null;
+		}
+	}
+	
+	public static void release(Connection conn,PreparedStatement pstmt,ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			rs = null;
+		}
+		release(conn, pstmt);
+	}
+}
--- jwxt/src/entity/Course.java
+++ jwxt/src/entity/Course.java
@@ -2,69 +2,70 @@ package entity;
 
 public class Course {
 
-	private String	cour_no ;
-	private int	cty_no ;
-	private String	cour_name ;
-	private double	cour_credit ;
-	private int	cour_ctime ;
-	private String	cour_open_team ;
-	private String	cour_is_require ;
-	private String	dept_no ;
-	private String	cour_remark ;
-	public String getCour_no() {
-		return cour_no;
+	private String	courNo ;
+	private int	ctyNo ;
+	private String	courName ;
+	private double	courRedit ;
+	private int	courCtime ;
+	private String	courOpenTeam ;
+	private String	courIsRequire ;
+	private String	deptNo ;
+	private String	courRemark ;
+	public Course() {
 	}
-	public void setCour_no(String cour_no) {
-		this.cour_no = cour_no;
+	public String getCourNo() {
+		return courNo;
 	}
-	public int getCty_no() {
-		return cty_no;
+	public void setCourNo(String courNo) {
+		this.courNo = courNo;
 	}
-	public void setCty_no(int cty_no) {
-		this.cty_no = cty_no;
+	public int getCtyNo() {
+		return ctyNo;
 	}
-	public String getCour_name() {
-		return cour_name;
+	public void setCtyNo(int ctyNo) {
+		this.ctyNo = ctyNo;
 	}
-	public void setCour_name(String cour_name) {
-		this.cour_name = cour_name;
+	public String getCourName() {
+		return courName;
 	}
-	public double getCour_credit() {
-		return cour_credit;
+	public void setCourName(String courName) {
+		this.courName = courName;
 	}
-	public void setCour_credit(double cour_credit) {
-		this.cour_credit = cour_credit;
+	public double getCourRedit() {
+		return courRedit;
 	}
-	public int getCour_ctime() {
-		return cour_ctime;
+	public void setCourRedit(double courRedit) {
+		this.courRedit = courRedit;
 	}
-	public void setCour_ctime(int cour_ctime) {
-		this.cour_ctime = cour_ctime;
+	public int getCourCtime() {
+		return courCtime;
 	}
-	public String getCour_open_team() {
-		return cour_open_team;
+	public void setCourCtime(int courCtime) {
+		this.courCtime = courCtime;
 	}
-	public void setCour_open_team(String cour_open_team) {
-		this.cour_open_team = cour_open_team;
+	public String getCourOpenTeam() {
+		return courOpenTeam;
 	}
-	public String getCour_is_require() {
-		return cour_is_require;
+	public void setCourOpenTeam(String courOpenTeam) {
+		this.courOpenTeam = courOpenTeam;
 	}
-	public void setCour_is_require(String cour_is_require) {
-		this.cour_is_require = cour_is_require;
+	public String getCourIsRequire() {
+		return courIsRequire;
 	}
-	public String getDept_no() {
-		return dept_no;
+	public void setCourIsRequire(String courIsRequire) {
+		this.courIsRequire = courIsRequire;
 	}
-	public void setDept_no(String dept_no) {
-		this.dept_no = dept_no;
+	public String getDeptNo() {
+		return deptNo;
 	}
-	public String getCour_remark() {
-		return cour_remark;
+	public void setDeptNo(String deptNo) {
+		this.deptNo = deptNo;
 	}
-	public void setCour_remark(String cour_remark) {
-		this.cour_remark = cour_remark;
+	public String getCourRemark() {
+		return courRemark;
 	}
-
+	public void setCourRemark(String courRemark) {
+		this.courRemark = courRemark;
+	}	
 	
 }
