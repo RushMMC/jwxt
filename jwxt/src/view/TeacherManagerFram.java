cddf1f61567498ae3643ae2016be86fe924f4dba
RushMMC <2759889206@qq.com>
2021-06-18 17:55:04 +0800

    增加教师管理

---
+++ jwxt/src/view/TeacherManagerFram.java
@@ -0,0 +1,107 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.CardLayout;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class TeacherManagerFram extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TeacherManagerFram frame = new TeacherManagerFram();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TeacherManagerFram() {
+		setTitle("\u6559\u5E08\u4FE1\u606F\u7BA1\u7406\u7A97\u4F53");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "name_25808716617982");
+		panel.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"\u6559\u5E08\u7F16\u53F7,\u6559\u5E08\u540D\u79F0"}));
+		comboBox.setBounds(82, 10, 107, 21);
+		panel.add(comboBox);
+		
+		textField = new JTextField();
+		textField.setBounds(199, 10, 99, 21);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("\u67E5\u8BE2");
+		btnNewButton.setBounds(308, 9, 93, 23);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\u67E5\u8BE2\u5168\u90E8");
+		btnNewButton_1.setBounds(411, 9, 93, 23);
+		panel.add(btnNewButton_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(82, 81, 422, 289);
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
+		JButton btnNewButton_2 = new JButton("\u67E5\u8BE2");
+		btnNewButton_2.setBounds(147, 406, 93, 23);
+		panel.add(btnNewButton_2);
+		
+		JButton btnNewButton_1_1 = new JButton("\u67E5\u8BE2\u5168\u90E8");
+		btnNewButton_1_1.setBounds(262, 406, 93, 23);
+		panel.add(btnNewButton_1_1);
+		
+		JButton btnNewButton_1_1_1 = new JButton("\u67E5\u8BE2\u5168\u90E8");
+		btnNewButton_1_1_1.setBounds(374, 406, 93, 23);
+		panel.add(btnNewButton_1_1_1);
+	}
+}
---
+++ jwxt/src/view/TecherAddFram.java
@@ -0,0 +1,93 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class TecherAddFram extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TecherAddFram frame = new TecherAddFram();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TecherAddFram() {
+		setTitle("\u8001\u5E08\u6DFB\u52A0fram");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("\u7CFB\u90E8\u7F16\u53F7\uFF1A");
+		lblNewLabel.setBounds(169, 64, 90, 15);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(300, 61, 150, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("\u7CFB\u90E8\u7F16\u53F7\uFF1A");
+		lblNewLabel_1.setBounds(169, 111, 90, 15);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(300, 108, 150, 21);
+		contentPane.add(textField_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("\u7CFB\u90E8\u7F16\u53F7\uFF1A");
+		lblNewLabel_1_1.setBounds(169, 166, 90, 15);
+		contentPane.add(lblNewLabel_1_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(300, 163, 150, 21);
+		contentPane.add(textField_2);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("\u7CFB\u90E8\u7F16\u53F7\uFF1A");
+		lblNewLabel_1_2.setBounds(169, 225, 90, 15);
+		contentPane.add(lblNewLabel_1_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(300, 222, 150, 21);
+		contentPane.add(textField_3);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(211, 347, 93, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(339, 347, 93, 23);
+		contentPane.add(btnNewButton_1);
+	}
+}
